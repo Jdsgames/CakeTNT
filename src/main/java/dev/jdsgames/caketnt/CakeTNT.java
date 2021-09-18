e35a1483b5eaf7c2c543cd14331b834fe9b7ff78
BuildTools <unconfigured@null.spigotmc.org>
2021-09-18 01:02:26 -0400

    Initial Plugin Setup

---
+++ src/main/java/dev/jdsgames/caketnt/CakeTNT.java
@@ -0,0 +1,17 @@
+package dev.jdsgames.caketnt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CakeTNT extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
